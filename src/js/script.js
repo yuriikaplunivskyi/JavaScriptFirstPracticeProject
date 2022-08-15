4875ee2df49c2484a1ddf183ec34605338872080
Yurii <yurii.kaplunivskyi@gmail.com>
2022-08-15 23:56:53 +0300

    This is first 'application'

---
+++ src/js/script.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const numberOfFilms = + prompt('Скільки фільмів Ви уже переглянули?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один з останніх переглянутіх фільмів?', ''),
+      b = prompt('Як оцінете цей фільм?', ''),
+      c = prompt('Один з останніх переглянутіх фільмів?', ''),
+      d = prompt('Як оцінете цей фільм?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
